c5fb09af51882b919f020248bebb235b835935fb
Yvan Yang <yvan.yang@outlook.com>
2017-10-19 00:09:22 +0800

    add new syntax

--- routes/one.js
+++ routes/one.js
@@ -1,9 +1,12 @@
+
+var Wechat =  require('node-wechat-sign')
+
 var express = require('express');
+require('babel-polyfill');
 
-
-var sign = require('../sign.js');
+//var sign = require('../sign.js');
 var wechatConfig = require('../wechat.conf.js');
-var axios = require('axios')
+//var axios = require('axios')
 
 var router = express.Router();
 
@@ -14,36 +17,45 @@ router.get('/one.do', function(req, res, next) {
 });
 
 
-function getConfig(url, response) {
-  let accessTokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${wechatConfig.appId}&secret=${wechatConfig.secret}`
-  axios.get(accessTokenUrl).then(res=>{
-      let accessToken = res.data.access_token;
-      console.log('accessToken:',accessToken)
+// function getConfig(url, response) {
+//   let accessTokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${wechatConfig.appId}&secret=${wechatConfig.secret}`
+//   axios.get(accessTokenUrl).then(res=>{
+//       let accessToken = res.data.access_token;
+//       console.log('accessToken:',accessToken)
 
-      let jsapiTicketUrl = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${accessToken}&type=jsapi`
-      axios.get(jsapiTicketUrl).then(res=>{
-          let jsapi_ticket = res.data.ticket;
-          let wxconf = sign(jsapi_ticket, url)
-          console.log('wxconfig:',wxconf)
+//       let jsapiTicketUrl = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${accessToken}&type=jsapi`
+//       axios.get(jsapiTicketUrl).then(res=>{
+//           let jsapi_ticket = res.data.ticket;
+//           let wxconf = sign(jsapi_ticket, url)
+//           console.log('wxconfig:',wxconf)
 
-          response.render('sign', { wxconf: wxconf});
-      }).catch(error=>{
-          console.log(error)
+//           response.render('sign', { wxconf: wxconf});
+//       }).catch(error=>{
+//           console.log(error)
    
-          response.render('sign', { wxconf});
-      })
-  }).catch(error=>{
-      console.log(error)
+//           response.render('sign', { wxconf});
+//       })
+//   }).catch(error=>{
+//       console.log(error)
     
-      response.render('sign', { wxconf: {}});
-  })
-}
+//       response.render('sign', { wxconf: {}});
+//   })
+// }
 
 /* GET home page. */
 router.get('/sign.do', function(req, res, next) {
   // res.render('sign', { });
   var url = req.query.url
-  getConfig(url, res) 
+  //getConfig(url, res) 
+  let config = {
+    appid: wechatConfig.appId,
+    secret: wechatConfig.secret
+  }
+ 
+  const wechat = new Wechat(config) 
+  const signature = await wechat.sign(url)
+  console.log('signature:', signature)
+  response.render('sign', { wxconf: signature});
 });
 
 
